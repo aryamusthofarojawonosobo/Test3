7bf568fba1335ea5a9af05bb36a067ba14d4211f
Azry <azry695@gmail.com>
2022-04-06 04:07:53 +0700

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,23 +3,22 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 ╭═════════════════
 ║╭──❉ *𝐁𝐈𝐎𝐃𝐀𝐓𝐀 𝐎𝐖𝐍𝐄𝐑* ❉───
-║│➸ *Nama* : Hyzer ( nama samaran ) 
-║│➸ *Umur* : 15
-║│➸ *Kelas* :  9 SMP
+║│➸ *Nama* : AzRy
+║│➸ *Umur* : 13
+║│➸ *Kelas* :  7 SMP
 ║│➸ *Status* : can't be good atau everything
 ║╰───────────────
 ╰════════════════
 ╭════════════════
 ║╭──❉ *𝐒𝐎𝐒𝐈𝐀𝐋 𝐌𝐄𝐃𝐈𝐀* ❉───
-║│➸ *No.Owner* : wa.me/6287892711054
-║│➸ *No.Bot* : wa.me/6287726311156
-║│➸ *Chanel Youtube* : https://youtube.com/channel/UC3Y7rHuI_OmbvFdL88JF7AQ
-║│➸ *Gmail* : Xnxx@gmail.com
-║│➸ *Github* : https://github.com/Hyzerr 
+║│➸ *No.Owner* : wa.me/6285795035419
+║│➸ *No.Bot* : wa.me/6285795035418
+║│➸ *Youtube* : 
+║│➸ *Gmail* : azry695@gmail.com
+║│➸ *Github* : https://github.com/AzRyCb
 ║╰────────────────
 ╰═════════════════
 _Oke udah itu aja terimakasih_
-
 `.trim()
 conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 

3ec56444e14eb81a685e98036c2dbb720aad1633
Azry <azry695@gmail.com>
2022-04-06 04:02:49 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,18 +1,22 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Minggu* :      *Rp 5.000*
-╠➥ *1 Bulan* :      *Rp 10.000*
-╠➥ *Permanen* : *Rp 25.000*
-╠➥ *Trial* :   *Rp 0*
-║
-╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
-╠➥ Topup/Pin
-║
-╠═ Tertarik Untuk Beli Bot Ini?
-╠➥Klil Button Di Bawah
-║
-╠═〘 Hyzer 〙 ═`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
+	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `
+╭═══ 〘 SEWA 〙
+║ ┅ ๑————————————๑
+║┊ ⌲ Sewa = 5k/grup (1 minggu)
+║┊ ⌲ Sewa = 15k/grup (1 bulan)
+║┊ ⌲ Sewa = 25k/grup (3 bulan)
+╰═ ┅ ═══════
+                
+༅ KEUNTUNGAN
+                
+✧ Bebas Add bot ke grup
+✧ bisa jaga grup kamu
+✧ Bisa nyuruh Buka/Tutup Group
+✧ bisa kick atau add seseorang
+✧ Ada fitur khusus admin
+✧ Ada fitur setting grup
+✧ Antilink, antispam dll`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
 }
 
 handler.command = /^sewabot$/i

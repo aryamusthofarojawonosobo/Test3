ff2312d678da2aacc2dc72035057fe5663af9914
Bang Developer Botz <101171085+devbotmail@users.noreply.github.com>
2022-04-09 09:37:40 +0700

    Update and rename plugins/add.js to plugin/add.js

--- plugin/add.js
+++ plugin/add.js
@@ -34,9 +34,9 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
 handler.help = ['add', '+'].map(v => v + ' nomor,nomor')
 handler.tags = ['admin']
 handler.command = /^(add|\+)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+handler.owner = true
+handler.mods = true
+handler.premium = true
 handler.group = true
 handler.private = false
 
